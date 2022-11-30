23181e6b6fd1e3fec9a88173c2951e8e27dd246a
Juan Jose <josemargri3@gmail.com>
2022-11-30 14:37:16 -0500

    First commit semana 3

---
+++ Semana-3/Ejercicio_4.py
@@ -0,0 +1,3 @@
+n = int(input())
+
+print(f'Tienes {n} lo siento pero no puedes hacer lo que quieras' if n < 18 else f'Tienes {n} sigues sin poder hacer lo que quieras pero ademas tienes obligaciones')
